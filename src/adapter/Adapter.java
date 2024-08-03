bdc3c8ef4610ae963643d17cffa60a6ee5c2a801
bprado <bastian.prado.v@gmail.com>
2024-08-03 09:48:17 -0700

    Add adapter pattern

---
+++ src/adapter/Adapter.java
@@ -0,0 +1,59 @@
+package adapter;
+
+// Target interface
+interface MediaPlayer {
+    void play(String audioType, String fileName);
+}
+
+// Adapter class
+class AdvancedMediaPlayer {
+    public void playVlc(String fileName) {
+        System.out.println("Playing vlc file. Name: " + fileName);
+    }
+
+    public void playMp4(String fileName) {
+        System.out.println("Playing mp4 file. Name: " + fileName);
+    }
+}
+
+// Adapter class
+class MediaAdapter implements MediaPlayer {
+    AdvancedMediaPlayer advancedMediaPlayer;
+
+    public MediaAdapter(String audioType) {
+        if (audioType.equalsIgnoreCase("vlc")) {
+            advancedMediaPlayer = new AdvancedMediaPlayer();
+        } else if (audioType.equalsIgnoreCase("mp4")) {
+            advancedMediaPlayer = new AdvancedMediaPlayer();
+        }
+    }
+
+    @Override
+    public void play(String audioType, String fileName) {
+        if (audioType.equalsIgnoreCase("vlc")) {
+            advancedMediaPlayer.playVlc(fileName);
+        } else if (audioType.equalsIgnoreCase("mp4")) {
+            advancedMediaPlayer.playMp4(fileName);
+        }
+    }
+}
+
+// Concrete implementation of the target interface
+class AudioPlayer implements MediaPlayer {
+    MediaAdapter mediaAdapter;
+
+    @Override
+    public void play(String audioType, String fileName) {
+        // inbuilt support to play mp3 music files
+        if (audioType.equalsIgnoreCase("mp3")) {
+            System.out.println("Playing mp3 file. Name: " + fileName);
+        }
+        // mediaAdapter is providing support to play other file formats
+        else if (audioType.equalsIgnoreCase("vlc") || audioType.equalsIgnoreCase("mp4")) {
+            mediaAdapter = new MediaAdapter(audioType);
+            mediaAdapter.play(audioType, fileName);
+        } else {
+            System.out.println("Invalid media. " + audioType + " format not supported");
+        }
+    }
+}
---
+++ src/adapter/README.md
@@ -0,0 +1,14 @@
+# Builder
+## Definition:
+The Adapter Pattern is used to allow two incompatible interfaces to work together. It acts as a bridge between two incompatible interfaces by wrapping an existing class with a new interface.
+
+## When to Use:
+- When you want to use an existing class but its interface is not compatible with the rest of your system.
+- When you need to integrate a third-party library that doesn’t match the required interface.
+- When you need to convert the interface of a class into another interface that clients expect.
+
+## Examples
+- When updating a software system, you might need to integrate old legacy components with new components. The Adapter Pattern allows you to create an adapter that sits between the legacy code and the new system, translating calls to the appropriate methods.
+- When using a third-party library that has a different interface from what your application expects, you can create an adapter to wrap the library's classes, providing a compatible interface for your application.
+- In scenarios where hardware interfaces (like device drivers) need to communicate with software systems, adapters can translate hardware-specific signals and commands into standardized software calls.
+- When integrating UI components from different libraries or frameworks, adapters can help translate the component interfaces so they can work seamlessly within a single application.
